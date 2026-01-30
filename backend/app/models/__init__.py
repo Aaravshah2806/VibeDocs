a742694a5d46489959fe315bb1bb3edec0cacd7a
LearnerAarav <shahaarav2806@gmail.com>
2026-01-30 07:52:43 +0530

    Docker

--- backend/app/models/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Database models."""
-from app.models.user import User
-from app.models.repository import Repository
-from app.models.generation import Generation
-
-__all__ = ["User", "Repository", "Generation"]
